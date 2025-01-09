ea12011f3dae7b17620b48a4c5251c643778dfc4
odogwukelly <odogwukellyice@gmail.com>
2025-01-09 18:23:11 +0100

    hot fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
+app.config['09053361954'] = os.environ.get("09053361954")
+
 
 from app.routes import *
--- config.py
+++ config.py
@@ -1,9 +1,2 @@
-import json
-
-with open('/etc/config.json') as config_file:
-    config = json.load(config_file)
-
-
 class Config:
     DEBUG = True
-    SECRET_KEY = config.get("SECRET_KEY")
